427fcf6569b9a43bcc3ade9bb32660a5e3533a71
Aayush patial <thakuraayush1522@gmail.com>
2026-06-23 10:47:57 +0530

    Created components

---
+++ components/src/App.jsx
@@ -0,0 +1,13 @@
+import Student from "./components/Student";
+import Person from "./components/Person";
+function App (){
+     return (
+        <>
+        <h1>Hello </h1>
+        <Student></Student>
+        <Person></Person>
+        </>
+     )
+}
+
+export default App;
---
+++ components/src/components/Student.jsx
@@ -0,0 +1,11 @@
+function Student (){
+    return (
+        <>
+            <h1>Name is Aayush Patial</h1>
+            <h2>Age is 20</h2>
+
+        </>
+    )
+}
+
+export default Student;
