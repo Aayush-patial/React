41f7ba16053be018124548f2882f8322b4f43e3c
Aayush patial <thakuraayush1522@gmail.com>
2026-06-24 09:47:53 +0530

    Created a webpage using React

---
+++ webpage/src/components/Body.jsx
@@ -0,0 +1,11 @@
+function Body (){
+    return (
+        <>
+        <img src="https://tse4.mm.bing.net/th/id/OIP.V1zVa5IUI22o0i6gG4or2QHaLH?cb=thumbexptrt1&urlcheck=1&rs=1&pid=ImgDetMain&o=7&rm=30" alt="Dog-image"></img>
+        <h1> This is a body of the webpage</h1>
+
+        </>
+    )
+}
+
+export default Body;
---
+++ webpage/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <>
+        <h3> Contact us:</h3>
+        <label> Your name:</label>
+        </>
+    )
+
+}
+
+export default Footer;
