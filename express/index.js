e0137937c0df05175e5cea279cb095f74d0f6ef5
Aayush patial <thakuraayush1522@gmail.com>
2026-07-02 10:52:30 +0530

    Created routes in express

--- express/index.js
+++ express/index.js
@@ -2,7 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+app.get("/student",(req,res)=>{
+    res.json({name:"Aayush patial",age:21});
+});
+app.get("/profile",(req,res)=>{
+    res.status(200).send("This is a profile page");
+}); 
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
-})
+});
