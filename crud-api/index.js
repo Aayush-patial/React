bd1a8116a1c305ee114733ff5bc3a30d0a6871c8
Aayush patial <thakuraayush1522@gmail.com>
2026-07-07 08:56:24 +0530

    Added put and delete route

--- crud-api/index.js
+++ crud-api/index.js
@@ -29,7 +29,7 @@ const students = [
         name:"Rohit kumar",
         class:"2nd",
         city:"Pune",
-        school:"Hiensenberg international school Pune"
+        school:"Hiensenberg international school California"
     }
 ];
 
@@ -38,18 +38,42 @@ app.get("/students",(req,res)=>{
     res.json(students);
 });
 
-app.get("/student/:id",(req,res)=>{
+app.get("/students/:id",(req,res)=>{
     const student = students.find(s => s.id === parseInt(req.params.id));
     if (!student) return res.status(404).send("Student not found");
     res.json(student);
 });
 
 app.post("/students",(req,res)=>{
-   students.push({id:5,name:"Aman patial",age:18});
+//    students.push({id:5,name:"Aman patial",age:18});
+    students.push(req.body);
    res.json({
     message:"Student added successfully",students
    });
 }) ;
+app.put("/students/:id",(req,res) =>{
+    const studentId = Number(req.params.id,10);
+
+    const index = students.findIndex(s => s.id === studentId);
+
+    students[index] =req.body;
+
+    res.json({
+        message : "Student updated successfully",students
+    });
+});
+
+app.delete("/students/:id" ,(req,res) =>{
+
+    const studentiId = Number(req.params.id,10);
+    
+    const new_students = students.filter(s=> s.id !== studentiId);
+
+    res.json ({
+        message:"Student deleted successfully", 
+       new_students
+    });
+});
 
 
 app.listen(3000,()=>{
